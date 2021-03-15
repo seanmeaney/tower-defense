9b6ff34a3833dedb502835fc41122c7475fe8464
Lily Barker <lilybarker@rinble.com>
2021-03-15 16:27:09 -0400

    fixed tower placement

--- COMP1501 - Tutorial 7 Code Base/map.py
+++ COMP1501 - Tutorial 7 Code Base/map.py
@@ -92,10 +92,8 @@ def render_map(map, screen, settings):
 
 def check_location(map, settings, location):
     if location[0] > 0 and location[0] < settings.window_size[1] and location[1] > 0 and location[1] < settings.window_size[0]-200:
-        test = (int(round(location[0]/20) * 20), int(round(location[1]/20) * 20))
-        if test in map.r_locations:
-            return False
-        else: 
+        print(f"location is {int(location[0]/20)}")
+        fixed_location = (int(location[0]/20), int(location[1]/20))
+        if map.map_data[fixed_location].type() == "wall":
             return True
-    else:
-        return False
+    return False
